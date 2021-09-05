fb4e696d5a1af2a0e8909af61040f2f7a5cf4659
AnshitaMathur <anshita.mathur@knoldus.com>
2021-09-05 22:14:33 +0530

    Initial commit

---
+++ src/main/java/System/User.java
@@ -0,0 +1,20 @@
+package System;
+
+public class User {
+    int id, age;
+    String name, course;
+    public User(int id, String name, int age, String course) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+
+    }
+
+    public String toString() {
+
+        return ("\"Id\":"+"\""+Integer.toString(id)+"\","+"\"Name\":"+"\""+name+"\","+"\"Age\":"+"\""+Integer.toString(age)+"\","+"\"Course\":"+"\""+course+"\"");
+    }
+
+}
